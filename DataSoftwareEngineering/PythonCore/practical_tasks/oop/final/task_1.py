8d5ef50aa6e8befbb9f0cd7af9c6058a8e238c97
abdibrokhim <abdibrokhim@gmail.com>
2024-06-09 13:42:44 +0500

    add examples

--- DataSoftwareEngineering/PythonCore/practical_tasks/oop/final/task_1.py
+++ DataSoftwareEngineering/PythonCore/practical_tasks/oop/final/task_1.py
@@ -7,11 +7,18 @@
 # >>> p is f
 # True
 
-class Sun:
-    _instance = None
+class Sun: # 1. `class Sun:` - This line is defining a new class named `Sun`.
+    _instance = None # 2. `_instance = None` - This is a class variable that will hold the single instance of the `Sun` class. It is initially set to `None`.
 
-    @classmethod
-    def inst(cls):
-        if cls._instance is None:
-            cls._instance = cls()
-        return cls._instance
+    @classmethod # 3. `@classmethod` - This is a decorator in Python that makes the method `inst()` a class method. Class methods receive the class as the first argument (conventionally named `cls`) instead of an instance (`self`).
+    def inst(cls): # 4. `def inst(cls):` - This is the definition of the class method `inst()`. This method will be used to create and return the single instance of the `Sun` class.
+        if cls._instance is None: # 5. `if cls._instance is None:` - This line checks if the `_instance` class variable is `None`. If it is, that means an instance of the `Sun` class has not been created yet.
+            cls._instance = cls() # 6. `cls._instance = cls()` - If `_instance` is `None`, this line creates a new instance of the `Sun` class and assigns it to `_instance`.
+        return cls._instance # 7. `return cls._instance` - This line returns the instance of the `Sun` class. If an instance already existed, it simply returns that. If not, it creates a new one and then returns it.
+
+# The purpose of this design pattern is to ensure that only one instance of the `Sun` class can ever be created. This can be useful in situations where you want to ensure that a certain resource or state is accessed globally and consistently across your program.
+
+# Example usage:
+p = Sun.inst()
+f = Sun.inst()
+print(p is f)
