a0e1dc4315e697f9c18226b3c3b1d6e0dcb10f91
abdibrokhim <abdibrokhim@gmail.com>
2024-06-19 14:20:44 +0500

    add initial final task

---
+++ DataSoftwareEngineering/PythonCore/practical_tasks/final/rss_xml/initial_task.py
@@ -0,0 +1,38 @@
+from argparse import ArgumentParser
+from typing import List, Optional, Sequence
+import requests
+import xml.etree.ElementTree as ET
+from json import dumps as json_dumps
+
+class UnhandledException(Exception):
+    pass
+
+def parse_rss_item(item, limit):
+    pass
+
+def main(argv: Optional[Sequence] = None):
+    """
+    The main function of your task.
+    """
+    parser = ArgumentParser(
+        prog="rss_reader",
+        description="Pure Python command-line RSS reader.",
+    )
+    parser.add_argument("source", help="RSS URL", type=str, nargs="?")
+    parser.add_argument(
+        "--json", help="Print result as JSON in stdout", action="store_true"
+    )
+    parser.add_argument(
+        "--limit", help="Limit news topics if this parameter provided", type=int
+    )
+
+    args = parser.parse_args(argv)
+    xml = requests.get(args.source).text
+    try:
+        print("\n".join(rss_parser(xml, args.limit, args.json)))
+        return 0
+    except Exception as e:
+        raise UnhandledException(e)
+
+if __name__ == "__main__":
+    main()
